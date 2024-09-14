4c2f0868f18972cdcdbae1581139ed41ee30bb14
Daniel Ivan Guerrero Ponce <danielivanguerreroponce@gamil.com>
2024-09-14 11:17:10 -0600

    PROYECTO TERMINADO

--- src/temas-ts/01-Areas.ts
+++ src/temas-ts/01-Areas.ts
@@ -1,20 +1,30 @@
 export class Areas{
     protected Cuadrado:number;
+    protected lado1:number;
     protected circulo:number;
     protected cuadradoArea:number;
     protected circuloArea:number;
-    constructor(Cuadrado:number, circulo: number, cuadradoArea:number, circuloArea:number){
+    protected RectArea:number;
+    private potencia:number=2;
+    constructor(Cuadrado:number, circulo: number, cuadradoArea:number, 
+        circuloArea:number, lado1:number, RectArea:number){
         this.Cuadrado=Cuadrado;
         this.circulo=circulo;
         this.cuadradoArea=cuadradoArea;
         this.circuloArea=circuloArea;
+        this.lado1=lado1;
+        this.RectArea=RectArea;
     }
     areaCua(){
-        this.cuadradoArea=this.Cuadrado*this.Cuadrado;
-        console.log(this.cuadradoArea)
+        this.cuadradoArea=Math.pow(this.Cuadrado, this.potencia)
+        return this.cuadradoArea
     }
     areaCir(){
-        this.circuloArea=3.1416*(this.circulo*this.circulo)
-        console.log(this.circuloArea)
+        this.circuloArea=Math.PI*Math.pow(this.circulo, this.potencia)
+        return this.circuloArea;
+    }
+    areaRect(){
+        this.RectArea=this.Cuadrado*this.lado1;
+        return this.RectArea;
     }
 }
--- src/temas-ts/02-Volumenes.ts
+++ src/temas-ts/02-Volumenes.ts
@@ -1,39 +1,33 @@
-import { Areas } from "./01-Areas";
+    import { Areas } from "./01-Areas";
 
-class Volumen extends Areas{
-    private altura:number;
-    private prisma:number;
-    private piramide:number;
-    private cilindro:number;
-    constructor(altura:number, Cuadrado:number, circulo:number, cuadradoArea:number, circuloArea:number, prisma:number, piramide:number, cilindro:number){
-        super(Cuadrado,circulo, cuadradoArea, circuloArea);
-        this.altura=altura;
-        this.prisma=prisma;
-        this.piramide=piramide;
-        this.cilindro=cilindro;
+    class Volumen extends Areas{
+        private altura:number;
+
+        constructor(altura:number, Cuadrado:number, circulo:number,
+            lado1:number, RectArea:number,
+            cuadradoArea:number, circuloArea:number ){
+            super(Cuadrado,circulo, cuadradoArea, circuloArea, lado1, RectArea);
+            this.altura=altura;
+        }
+        
+        Prisma(){
+            //this.prisma=this.areaCua()*this.altura;
+            console.log(`El volumen del prisma es de: ${this.areaCua()*this.altura} cm*3`)
+        }
+        Piramide(){
+            //his.piramide=(this.areaRect()*this.altura)/3
+            console.log(`El volumen de la piramide es de: ${(this.areaRect()*this.altura)/3}cm*3`)
+        }
+        Cilindro(){
+            //this.cilindro=this.areaCir()*this.altura
+            console.log(`el volumen del cilindro es de: ${this.areaCir()*this.altura}cm*3`)
+        }
     }
-    areaCua(): void {
-        console.log(`El area del cuadrado es de: ${this.cuadradoArea}`)
-    }
-    areaCir(): void {
-        console.log(`el area del circulo es de: ${this.circuloArea}`)
-    }
-    Prisma(){
-        this.prisma=this.cuadradoArea*this.altura;
-        console.log(`El volumen del prisma es de: ${this.prisma}`)
-    }
-    Piramide(){
-        this.piramide=(this.cuadradoArea*this.altura)/3
-        console.log(`El volumen de la piramide es de: ${this.piramide}`)
-    }
-    Cilindro(){
-        this.cilindro=this.circuloArea*this.altura
-        console.log(`el volumen del cilindro es de: ${this.cilindro}`)
-    }
-}
 
 
-let volumen1=new Volumen(10,4,5,16,78.53999999999999,0,0,0);
-volumen1.Prisma();
-volumen1.Piramide();
-volumen1.Cilindro();
+    let volumen1=new Volumen(10,4,8,5,0,0,0);
+    volumen1.areaCua();
+    volumen1.areaCir();
+    volumen1.Prisma();
+    volumen1.Piramide();
+    volumen1.Cilindro();
